d2ebfa015b83022139555d788deb54bc7b0bd73d
Freddy <freddy.wei0810@gmail.com>
2022-06-25 02:06:29 +0800

    feat: add good_recursion_1.py

---
+++ p1_2_good_recursion_1.py
@@ -0,0 +1,24 @@
+'''
+    Good recursion 1
+    However, Python does not support a tail-recursion optimization.
+    see: 
+        1. http://neopythonic.blogspot.com/2009/04/tail-recursion-elimination.html
+        2. https://stackoverflow.com/questions/13591970/does-python-optimize-tail-recursion
+'''
+
+s = 0
+
+
+def sum_next(lt, i):
+    global s
+
+    if i == len(lt): return 0
+    s += lt[i]
+    sum_next(lt, i+1)
+
+
+if __name__ == "__main__":
+    lt = [1 for _ in range(250000)]
+    sum_next(lt, 0)
+    print(s)
+    

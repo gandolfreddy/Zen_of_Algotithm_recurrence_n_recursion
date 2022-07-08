8a2947cdd60bd79730931610a0d93874c69dae07
Freddy <freddy.wei0810@gmail.com>
2022-07-08 16:46:46 +0800

    feat: modify p2_2_find_paths_1.py

--- p2_2_find_path_1.py
+++ p2_2_find_path_1.py
@@ -3,27 +3,26 @@
 '''
 
 
-from logging import root
-from ds_tools import Node, build_tree
+from ds_tools import build_tree
 
 
-def find_paths(node: Node, path: list[Node], paths: list[list[Node]]):
+def find_paths(node, path, paths):
     path.append(node)
-    if not node.left and node.right and not 21 % node.val:
+    if not node.left and not node.right and not 21 % node.val:
         paths.append(path)
     if node.left:
-        find_paths(node.left, [path], paths)
+        find_paths(node.left, path.copy(), paths)
     if node.right:
-        find_paths(node.right, [path], paths)
+        find_paths(node.right, path.copy(), paths)
 
 
 if __name__ == "__main__":
     lt = [1, 2, 3, 4, 5, 6, 7]
     root = build_tree(lt, 0, len(lt)-1)
-    paths = list(list())
+
+    paths = []
 
     find_paths(root, [], paths)
     for path in paths:
         vals = [node.val for node in path]
         print(vals)
-        

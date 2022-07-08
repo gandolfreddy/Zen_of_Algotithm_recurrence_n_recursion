801c9649a0056d6ad13a41ea351d411ae551046d
Freddy <freddy.wei0810@gmail.com>
2022-07-08 17:59:04 +0800

    feat: modify p2_2_find_paths_4.py

--- p2_2_find_paths_4.py
+++ p2_2_find_paths_4.py
@@ -11,23 +11,23 @@ def find_paths(root):
     cnts = {}
     path = Stack()
 
+    ## cnts: {root: 0}
     cnts[root] = 0
+    ## path: Stack(root)
     path.push(root)
 
     while True:
         top = path.peek()
         if cnts.get(top) == 0:
-            if not top.left:
-                cnts[top] = 1
+            if not top.left: cnts[top] = 1
             else:
-                path.push(top.left)
                 cnts[top.left] = 0
+                path.push(top.left)
         elif cnts.get(top) == 1:
-            if not top.right:
-                cnts[top] = 2
+            if not top.right: cnts[top] = 2
             else:
-                path.push(top.right)
                 cnts[top.right] = 0
+                path.push(top.right)
         elif cnts.get(top) == 2:
             if not top.left and not top.right and not 21 % top.val:
                 paths.append(list(path))

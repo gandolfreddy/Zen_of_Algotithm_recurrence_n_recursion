2815821a0b009b3d974651a9c4661f08e3e9150b
Freddy <freddy.wei0810@gmail.com>
2024-10-20 03:13:16 +0800

    Refactor get_path_sums function in p1_2_recurrence_implement_with_recurrence_get_path_sums_2.py

--- p1_2_recurrence_implement_with_recurrence_get_path_sums_2.py
+++ p1_2_recurrence_implement_with_recurrence_get_path_sums_2.py
@@ -3,12 +3,13 @@
 '''
 
 
-from p1_2_prepare_a_binary_tree import build_tree
+from ds_tools import build_tree
 
 
 def get_path_sums(root):
-    sums = dict()
-    node_q = list()
+    ''' Get the path sums of a binary tree '''
+    sums = {}
+    node_q = []
 
     sums[root] = root.val
     node_q.append(root)
@@ -17,15 +18,15 @@ def get_path_sums(root):
         cur_node = node_q.pop(0)
         cur_sum = sums[cur_node]
 
-        ## 去除不完全路徑和
+        # 去除不完全路徑和
         # if cur_node.left != None or cur_node.right != None:
         #     sums.pop(cur_node)
 
-        if cur_node.left != None:
+        if cur_node.left is not None:
             node_q.append(cur_node.left)
             sums[cur_node.left] = cur_sum + cur_node.left.val
 
-        if cur_node.right != None:
+        if cur_node.right is not None:
             node_q.append(cur_node.right)
             sums[cur_node.right] = cur_sum + cur_node.right.val
 
@@ -34,26 +35,27 @@ def get_path_sums(root):
 
 if __name__ == "__main__":
     lt = [1, 2, 3, 4, 5, 6, 7]
-    root = build_tree(lt, 0, len(lt)-1)
+    test_root = build_tree(lt, 0, len(lt)-1)
 
-    ## Binary Tree
-    ##        4
-    ##      /   \
-    ##     2     6
-    ##    / \   / \
-    ##   1   3 5   7
+    # Binary Tree
+    # 4
+    # /   \
+    # 2     6
+    # / \   / \
+    # 1   3 5   7
 
-    ## print the tree
+    # print the tree
     def print_tree(root):
-        if root == None: return
+        ''' Print the binary tree '''
+        if root is None:
+            return
         print(root.val, end=" ")
         print_tree(root.left)
         print_tree(root.right)
 
-    print_tree(root)
+    print_tree(test_root)
     print()
-        
-    sums = get_path_sums(root)
-    for k, v in sums.items():
-        print(f"Road to {k.val}: {v}")
 
+    test_sums = get_path_sums(test_root)
+    for k, v in test_sums.items():
+        print(f"Road to {k.val}: {v}")
